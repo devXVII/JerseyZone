e46eb6e8b1eb6a7fa41b5815af9e1c442e745a89
Talin Sharma <talinsharma.dev@gmail.com>
2022-03-09 15:11:10 -0500

    Update locations.js

--- src/locations.js
+++ src/locations.js
@@ -1,4 +1,8 @@
 var locations;
+window.onerror = function(msg, url, linenumber) {
+    alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
+    return true;
+}
 
 var template;
 window.onload = async () => {
